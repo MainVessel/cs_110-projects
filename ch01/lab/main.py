695182d6811198981e0f6a37abaf5d6f93f21592
ceruleancloud <emkonadu438@gmail.com>
2026-09-04 10:37:42 -0400

    Lab 1 complete

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,20 +1,29 @@
 import random
-
 # Part A
 def main():
+    weeks = 16
+    classes = 5
+    tuition = 6000
+    cost_per_week = (tuition / classes) / weeks
+    print("Cost per week:", cost_per_week)
+    classes_per_week = 3
+    cost_per_class = (cost_per_week/classes_per_week)
 
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = (tuition / classes) / weeks
-print("Cost per week:", cost_per_week)
+    print(weeks, type(weeks))
+    print(classes, type(classes))
+    print(tuition, type(tuition))
+    print(cost_per_week, type(cost_per_week))
+    print(classes_per_week, type(classes_per_week))
+    print(cost_per_class, type(cost_per_class))
 
-snacks = ["pizza", "apple", "cookies", "chips", "soda"]
+    print("The cost per class is:", cost_per_class)
 
-print(snacks, type(snacks))
-random_snack = random.choice(snacks)
-type(random_snack)
+    snacks = ["pizza", "apple", "cookies", "chips", "soda"]
 
-print("The randomly selected snack is", random_snack)
+    print(snacks, type(snacks))
+    random_snack = random.choice(snacks)
+    type(random_snack)
+
+    print("The randomly selected snack is", random_snack)
 
 main()
