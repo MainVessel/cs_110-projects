ff5f311a62659aa292e49598c63a2dc0a99b08ef
Emmanuel Amoateng-Konadu <emkonadu438@gmail.com>
2026-09-04 14:13:40 +0000

    lab part 2 remote

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,6 +1,20 @@
+import random
+
 # Part A
+def main():
+
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = (tuition / classes) / weeks
 print("Cost per week:", cost_per_week)
+
+snacks = ["pizza", "apple", "cookies", "chips", "soda"]
+
+print(snacks, type(snacks))
+random_snack = random.choice(snacks)
+type(random_snack)
+
+print("The randomly selected snack is", random_snack)
+
+main()
